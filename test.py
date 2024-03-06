51f0a4c7bf2e213317cac9223662f241155c438f
Shahnawaz-Sk <shahnawaz@lambdatest.com>
2024-03-06 13:33:28 +0530

    test

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import os
 
 from appium import webdriver
 
-
 caps = {
 
   "lt:options": {
     "w3c": True,
+    "deviceName": "iPhone 14 Plus",
     "platformName": "ios",
-    "deviceName" : ".*",
+    "platformVersion": "16",
     "isRealMobile": True,
     "app": "lt://APP1016043281709659296280310",  # Replace this with the APP ID of your uploaded app
     "build": "Example Test ",
@@ -20,17 +20,15 @@ caps = {
   }
 
 }
-
-
-gridUrl = "mobile-hub.lambdatest.com/wd/hub"
-
 username = os.getenv("LT_USERNAME")
 password = os.getenv("LT_ACCESS_KEY")
 
+
+gridUrl = "mobile-hub.lambdatest.com/wd/hub"
 
 url = "https://"+username+":"+password+"@"+gridUrl
 driver = webdriver.Remote(desired_capabilities = caps, command_executor = url)
 print("driver created")
 driver.execute_script("lambda-status=passed")
-time.sleep(10)
+time.sleep(17)
 driver.quit()
